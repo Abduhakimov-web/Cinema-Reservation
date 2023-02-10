99a8854976b6c7ff71b9a6afc7f5b788b369a401
Abdukhakimov Kurbon <qurbonabduhakimov12@gmail.com>
2023-02-10 23:24:39 +0500

    Copying all files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
-module com.example.cinemaresver {
+module com.example.cinemareservationver2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.mail;
+    requires java.desktop;
 
 
-    opens com.example.cinemaresver to javafx.fxml;
-    exports com.example.cinemaresver;
+    opens com.example.cinemareservationver2 to javafx.fxml;
+    exports com.example.cinemareservationver2;
+    exports com.example.cinemareservationver2.controllers;
+    opens com.example.cinemareservationver2.controllers to javafx.fxml;
 }
